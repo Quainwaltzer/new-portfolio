8e98b52c4da46893af7e775fc34e58f50e2f44aa
Quainwaltzer <reikoaisha2630@gmail.com>
2026-06-13 00:32:53 +0800

    Fixed issue in Vercel through Svelte's Server Side Rendering Node-Fetch conflict

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	ssr: {
+		// 🟢 Add this to stop Vite from trying to bundle node-fetch internals
+		noExternal: [],
+		external: ['node-fetch']
+	}
 });
